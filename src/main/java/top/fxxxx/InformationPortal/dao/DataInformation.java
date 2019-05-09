5494ee5d166c0a02ce4b6ca9d25f099d0279c25d
mofanyunxiang <1543382944@qq.com>
2019-05-09 22:32:14 +0800

    Initial commit

---
+++ src/main/java/top/fxxxx/InformationPortal/dao/DataInformation.java
@@ -0,0 +1,8 @@
+package top.fxxxx.InformationPortal.dao;
+
+class DataInformation {
+    public static String driver = "com.mysql.cj.jdbc.Driver";
+    public static String url = "jdbc:mysql://localhost:3306/testInformationPortal?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true";
+    public static String username = "root";
+    public static String password = "123456";
+}
